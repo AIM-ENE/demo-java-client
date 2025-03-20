c687715435bcc641cdd560b826979bb19f06985c
Bart <bart.vanderwal@han.nl>
2025-03-20 21:55:54 +0100

    Ook restassured variant gemaakt naast UniRest (wel data uit Cairo i.p.v. Elst o.i.d.?).

--- src/main/java/han/aim/se/javaclient/WeatherServiceRestAssured.java
+++ src/main/java/han/aim/se/javaclient/WeatherServiceRestAssured.java
@@ -1,25 +1,31 @@
 package han.aim.se.javaclient;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class WeatherServiceRestAssured extends WeatherService {
 
     @Override
-    public void getWeatherFor() throws UnirestException {
+    public void getWeatherFor()  {
 
         var request = currentWeatherEndpoint + coordinate;
 
-        HttpResponse<String> response = Unirest.get(request)
-                .header("x-rapidapi-key", apiKey)
-                .header("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
-                .asString();
+        // Make the request using RestAssured
+        Response response = RestAssured.given()
+        .header("x-rapidapi-key", apiKey)
+        .header("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+        .when()
+        .get(request)
+        .then()
+        .extract()
+        .response();
 
         // {"location":{"name":"Elst","region":"Gelderland","country":"Netherlands","lat":51.89,"lon":5.9}
         // System.out.println(response.getBody());
+        System.out.println("Response Code: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
         var parser = new WeatherJsonParser();
-        parser.getLocationAndTemperature(response.getBody());
+        parser.getLocationAndTemperature(response.getBody().asString());
     }
 
 }
